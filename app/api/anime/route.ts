da2be3e384c5a37f8ffc92a4b38fe7fe4f0435d7
magneum <krakinzkon@gmail.com>
2023-03-03 00:34:58 +0530

    magneum_next13.git

--- app/api/anime/route.ts
+++ app/api/anime/route.ts
@@ -1,3 +1,15 @@
 export async function GET(request: Request) {
-  return new Response("Hello, Next.js!");
+  return new Response("GET: Hello, Next.js!");
 }
+
+// export async function POST(request: Request) {
+// return new Response("POST: Hello, Next.js!");
+// }
+
+// export async function DELETE(request: Request) {
+// return new Response("DELETE: Hello, Next.js!");
+// }
+
+// export async function UPDATE(request: Request) {
+// return new Response("UPDATE: Hello, Next.js!");
+// }
