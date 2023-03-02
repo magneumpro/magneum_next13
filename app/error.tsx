27bca44dc4ad270c01cdcf9a15e0269311be7719
magneum <krakinzkon@gmail.com>
2023-03-03 00:32:18 +0530

    magneum_next13.git

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,7 @@
 export default function Error({ error }: { error: Error; reset: () => void }) {
   return (
     <div>
+      <h1>Something went wrong!</h1>
       <h2>{JSON.stringify(error)}</h2>
     </div>
   );
