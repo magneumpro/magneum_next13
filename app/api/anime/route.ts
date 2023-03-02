fddcde69387f68ef0e06c5779c431007c64c9ec2
magneum <krakinzkon@gmail.com>
2023-03-03 00:37:02 +0530

    magneum_next13.git

--- app/api/anime/route.ts
+++ app/api/anime/route.ts
@@ -1,17 +1,17 @@
 import { v4 as uuidv4 } from "uuid";
 
-export async function GET(request: Request) {
+export async function GET(request: Request, response: Response) {
   return new Response("GET: Hello, Next.js!");
 }
 
-// export async function POST(request: Request) {
+// export async function POST(request: Request, response: Response) {
 // return new Response("POST: Hello, Next.js!");
 // }
 
-// export async function DELETE(request: Request) {
+// export async function DELETE(request: Request, response: Response) {
 // return new Response("DELETE: Hello, Next.js!");
 // }
 
-// export async function UPDATE(request: Request) {
+// export async function UPDATE(request: Request, response: Response) {
 // return new Response("UPDATE: Hello, Next.js!");
 // }
