7fdea711aedc4394ec970751a7d018be8ce91301
magneum <krakinzkon@gmail.com>
2023-03-03 01:42:30 +0530

    magneum_next13.git

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
   return (
-    <div className="snap-y snap-mandatory">
-      <section className="Hero"></section>
-      <section className="About"></section>
-      <section className=""></section>
-      <section className=""></section>
+    <div className="snap-y snap-mandatory bg-[#282828] h-screen">
+      <section className="snap-start">
+        <div>Hero Section</div>
+      </section>
+      <section className="snap-start">
+        <div>About Section</div>
+      </section>
     </div>
   );
 }
