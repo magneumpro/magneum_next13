aec359ab484d2fa78fd34ea7eefc8590e241a333
magneum <krakinzkon@gmail.com>
2023-03-03 10:41:01 +0530

    magneum_next13.git

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js",
-    "./docs/**/*.{js,ts,jsx,tsx,html}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
