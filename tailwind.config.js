00f813a2d1d2062f3f286236c4845fa99635f19b
magneum <krakinzkon@gmail.com>
2023-03-03 10:35:17 +0530

    magneum_next13.git

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
-"./app/**/*.{js,ts,jsx,tsx}",
-"./pages/**/*.{js,ts,jsx,tsx}",
-"./node_modules/flowbite/**/*.js",
-"./components/**/*.{js,ts,jsx,tsx}",
-],
-theme: {
-extend: {},
-},
-plugins: [require("daisyui"), require("flowbite/plugin")],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    "./docs/**/*.{js,ts,jsx,tsx,html}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui"), require("flowbite/plugin")],
 };
