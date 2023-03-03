a46ced99dbf3df5f8681697c5b1bc2d689d0797c
magneum <krakinzkon@gmail.com>
2023-03-03 10:58:07 +0530

    magneum_next13.git

--- pages/api/manga.ts
+++ pages/api/manga.ts
@@ -22,60 +22,60 @@ export default async function test(req: NextApiRequest, res: NextApiResponse) {
       var manga = new Manga();
       var argument: any = req.query.q;
       var response = await manga.searchManga(argument);
-      if (!response) {
-        res.send({
-          status: "Failed with error code 911",
-          message: "Parameters requirement not met.",
-          date_create: moment().format("DD-MM-YYYY hh:mm:ss"),
+      if (!response.data) {
+        return res.status(500).json({
+          id: uuidv4(),
+          status: false,
+          message: "Server error.",
+          timestamp: moment().format("DD-MM-YYYY hh:mm:ss"),
           usage: {
             endpoint: "/api/manga?q=",
             example: "/api/manga?q=My Hero Academia by Kohei Horikoshi",
           },
         });
-      } else {
-        let Genres = "";
-        let Authors = "";
-        for (var i = 0; i < response.data[0].genres.length; i++) {
-          Genres += `${response.data[0].genres[i].name}`;
-        }
-        for (var i = 0; i < response.data[0].authors.length; i++) {
-          Authors += `name=>${response.data[0].authors[i].name} | type=>${response.data[0].authors[0].type}`;
-        }
-        var Found = [
-          {
-            status: true,
-            uuid: uuidv4(),
-            date_create: moment().format("DD-MM-YYYY hh:mm:ss"),
-            topic: "Manga Search",
-            query: argument,
-            title: response.data[0].title,
-            chapters: response.data[0].chapters,
-            published_on: response.data[0].published.from,
-            score: response.data[0].scored,
-            popularity: response.data[0].popularity,
-            favorites: response.data[0].favorites,
-            url: response.data[0].url,
-            genres: Genres || null,
-            authors: Authors || null,
-            background: response.data[0].background || null,
-            image: shorten(response.data[0].images.jpg.large_image_url),
-            description: response.data[0].synopsis,
-          },
-        ];
-        logger.info(Found);
-        return res.send(Found);
       }
-    } else {
-      return res.send({
-        status: "Failed with error code 911",
-        message: "Parameters requirement not met.",
-        date_create: moment().format("DD-MM-YYYY hh:mm:ss"),
+      let Genres = "";
+      let Authors = "";
+      for (var i = 0; i < response.data[0].genres.length; i++) {
+        Genres += `${response.data[0].genres[i].name}`;
+      }
+      for (var i = 0; i < response.data[0].authors.length; i++) {
+        Authors += `NAME: ${response.data[0].authors[i].name} | TYPE: ${response.data[0].authors[0].type}`;
+      }
+      return res.status(200).json({
+        response: {
+          id: uuidv4(),
+          status: true,
+          timestamp: moment().format("DD-MM-YYYY hh:mm:ss"),
+        },
+        meta: {
+          topic: "MANGA: finder",
+          query: argument,
+          title: response.data[0].title,
+          chapters: response.data[0].chapters,
+          published_on: response.data[0].published.from,
+          score: response.data[0].scored,
+          popularity: response.data[0].popularity,
+          favorites: response.data[0].favorites,
+          url: response.data[0].url,
+          genres: Genres || null,
+          authors: Authors || null,
+          background: response.data[0].background || null,
+          image: response.data[0].images.jpg.large_image_url,
+          description: response.data[0].synopsis,
+        },
+      });
+    } else
+      return res.status(500).json({
+        id: uuidv4(),
+        status: false,
+        message: "Arguments not satisfied.",
+        timestamp: moment().format("DD-MM-YYYY hh:mm:ss"),
         usage: {
           endpoint: "/api/manga?q=",
           example: "/api/manga?q=My Hero Academia by Kohei Horikoshi",
         },
       });
-    }
   } catch (error: any) {
     logger.error(error.message);
     return res.status(500).json({
